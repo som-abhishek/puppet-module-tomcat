39867c6c6260c2f8571906d4a230f6faaa3586a7
Abhishek Som <som.abhishek1994@yahoo.com>
2018-12-03 20:37:28 +0530

    Removed checksum code from deploy.pp

--- manifests/deploy.pp
+++ manifests/deploy.pp
@@ -1,16 +1,14 @@
 define tomcat::deploy(
  $deploy_path   =   $::tomcat::deploy_path,
  $deploy_url,
- $checksum = 'md5',
- $checksum_value,
+ 
 ){
     file{"${deploy_path}/${name}.war":
         source  =>  "${deploy_url}",
         owner   =>  $::tomcat::user,
         group   =>  $::tomcat::group,
         notify  =>  Exec['purge_context'],
-        checksum        =>  "${checksum}",
-        checksum_value  =>  "${checksum_value}",
+        
     
     }
     
